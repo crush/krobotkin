c776e5d61903866b56c2059550261ecddc20ccbb
Crush <crush.dev.tf@gmail.com>
2019-11-10 00:45:19 -0500

    Starting a directory of integration tests

---
+++ tests/integration/test_db.py
@@ -0,0 +1,18 @@
+import sqlite3 as sqlite
+
+import krobotkin.db as db
+
+
+class TestDB:
+    @classmethod
+    def setup_class(cls):
+        return
+
+
+    @classmethod
+    def teardown_class(cls):
+        return
+
+
+    def test_poll_simple_create_retrieve(self):
+        assert True
